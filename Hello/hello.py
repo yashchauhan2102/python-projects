dbc2882fe03e722720933c8f692ddcc67944eef7
yashchauhan2102 <Ychauhan@2102>
2024-04-28 23:44:52 +0530

    initial commit

---
+++ Hello/hello.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+
+#this line is for displaying a message
+print("Single line")
+
+'''print("Multiple, Line!1")
+print("Multiple, Line!2")
+print("Multiple, Line!3")
+print("Multiple, Line!4")
+print("Multiple, Line!5")
+'''
+
+print("Bhavya")
+
